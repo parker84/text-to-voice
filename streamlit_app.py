c630a49c0cc4d50a9a84b09a37a4c15e0d3c5e11
parker84 <brydonparker4@gmail.com>
2024-02-28 08:37:40 -0500

    v1 of app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,15 @@ from openai import OpenAI
 import os
 
 st.set_page_config(
-    page_title="Audio -> Text",
-    page_icon="🎧",
+    page_title="Text -> Voice",
+    page_icon="🔊",
 )
-st.title('🎧 Audio -> Text')
-st.caption('Upload an mp3 file and get the text from it!') 
+st.title('🔊 Text -> Voice')
+st.caption('Enter some text and turn it into a voice!') 
 
 
-st.sidebar.subheader("Enter Your API Key 🗝️")
 open_api_key = st.sidebar.text_input(
-    "Open API Key", 
+    "Enter Your OpenAI API Key 🗝️",
     value=st.session_state.get('open_api_key', ''),
     help="Get your API key from https://openai.com/",
     type='password'
@@ -20,20 +19,49 @@ open_api_key = st.sidebar.text_input(
 os.environ["OPENAI_API_KEY"] = open_api_key
 st.session_state['open_api_key'] = open_api_key
 
-audio_file = st.file_uploader("Choose an mp3 file", type="mp3")
+with st.sidebar.expander("⚙️ Settings"):
+    voice = st.selectbox(
+        "Voice Options 🗣️",
+        [
+            "nova",
+            "alloy",
+            "echo",
+            "fable",
+            "onyx",
+            "shimmer"
+        ],
+        help="Choose the voice you want to use. Test out the voices here: https://platform.openai.com/docs/guides/text-to-speech"
+    )
 
-if audio_file is not None:
-    st.audio(audio_file, format='audio/mp3')
+    model = st.selectbox(
+        "Model Options 🤖",
+        ["tts-1", "tts-1-hd"],
+        help="Choose the model you want to use for the voice. Compare prices here: https://openai.com/pricing"
+    )
 
+with st.form(key='text_form'):
+    text_input = st.text_area("Enter Text", "Hello, how are you?")
+    submit_button = st.form_submit_button(label='Generate Audio 🎵', type="primary")
+
+if submit_button is not None:
     if open_api_key == '' or open_api_key is None:
         st.error("⚠️ Please enter your API key in the sidebar")
     else:
-        with st.spinner('Transcribing...'):
-            client = OpenAI(
-                api_key=open_api_key
+        client = OpenAI(
+            api_key=open_api_key
+        )
+        with st.spinner('Generating audio...'):
+            response = client.audio.speech.create(
+                model=model,
+                voice=voice,
+                input=text_input
             )
-            transcription = client.audio.transcriptions.create(
-                model="whisper-1", 
-                file=audio_file
+            response.write_to_file("output.mp3")
+        with open("output.mp3", "rb") as audio_file:
+            st.audio(audio_file, format='audio/mp3')
+            st.download_button(
+                label="⬇️ Download Audio",
+                data=audio_file,
+                file_name="output.mp3",
+                mime="audio/mp3"
             )
-        st.write(f'**Text**: {transcription.text}')
